feae1d02dfec798ae1c06f73c4a679c337c4f3f0
Larry Ullman <Larry@LarryUllman.com>
2017-04-23 15:05:53 -0400

    add file for book

--- ch01/constants.php
+++ ch01/constants.php
@@ -8,7 +8,7 @@
 <?php # Script 1.9 - constants.php
 
 // Set today's date as a constant:
-define ('TODAY', 'March 16, 2011');
+define('TODAY', 'April 23, 2017');
 
 // Print a message, using predefined constants and the TODAY constant:
 echo '<p>Today is ' . TODAY . '.<br>This server is running version <strong>' . PHP_VERSION . '</strong> of PHP on the <strong>' . PHP_OS . '</strong> operating system.</p>';
